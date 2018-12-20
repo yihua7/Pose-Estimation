3d38d82ce40c4ad5e17c9fa96848f69d0482f540
HYH07007 <35869256+HYH07007@users.noreply.github.com>
2018-12-21 01:06:28 +0800

    update

--- train.py
+++ train.py
@@ -26,5 +26,10 @@ model = Stacked_Hourglass(block_number=block_number, layers=layers, out_dim=out_
 
 cycle = 10000
 
-# model.train(image_path, label_path, batch_size, cycle+1, False, 0, step='all', data_set='augment')
+image_path = data_set_path + 'augmented_dataset\\images_random_padding\\'
+label_path = data_set_path + 'augmented_dataset\\'
+model.train(image_path, label_path, batch_size, cycle+1, False, 0, step='all', data_set='augment')
+
+image_path = data_set_path + 'video_frame_resize\\'
+label_path = data_set_path + 'video_frame_resize\\'
 model.train(image_path, label_path, batch_size, 2*cycle+1, False, cycle, step='all', data_set='Didi')
